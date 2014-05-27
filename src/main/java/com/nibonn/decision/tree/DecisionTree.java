0b155b5b9437a19773f1f1fc078521d15ea8c0f0
nibonn <516860685@qq.com>
2014-05-27 13:20:52 +0800

    add load data

--- src/main/java/com/nibonn/decision/tree/DecisionTree.java
+++ src/main/java/com/nibonn/decision/tree/DecisionTree.java
@@ -1,5 +1,9 @@
 package com.nibonn.decision.tree;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 /**
  * Hello world!
  */
@@ -8,7 +12,43 @@ public class DecisionTree {
         System.out.println("Hello World!");
     }
 
-    public static class Builder {
+    private Node root;
 
+    private DecisionTree() {
+        root = new Node();
+    }
+
+    public static class DecisionTreeBuilder {
+        private Double[][] data;
+
+        public void loadData(String filename, int dimension, int size) throws FileNotFoundException {
+            if (dimension <= 0 || size <= 0) {
+                throw new IllegalArgumentException("dimension and size must greater than zero.");
+            }
+            data = new Double[size][dimension];
+            Scanner in = new Scanner(new File(filename));
+            for (int i = 0; i < size; ++i) {
+                for (int j = 0; j < dimension; ++j) {
+                    data[i][j] = in.nextDouble();
+                }
+                in.nextLine();
+            }
+            in.close();
+        }
+
+        public DecisionTree build() {
+            // TODO build tree
+            return null;
+        }
+
+        public Double[][] getData() {
+            return data;
+        }
+    }
+
+    private class Node {
+        boolean isLeaf = false;
+        Node lSon;
+        Node rSon;
     }
 }
--- src/test/java/com/nibonn/decision/tree/DecisionTreeTest.java
+++ src/test/java/com/nibonn/decision/tree/DecisionTreeTest.java
@@ -1,34 +1,35 @@
 package com.nibonn.decision.tree;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.FileNotFoundException;
 
 /**
  * Unit test for simple App.
  */
-public class DecisionTreeTest
-        extends TestCase {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public DecisionTreeTest(String testName) {
-        super(testName);
+public class DecisionTreeTest {
+
+    private static DecisionTree.DecisionTreeBuilder builder;
+
+    @BeforeClass
+    public static void setup() {
+        builder = new DecisionTree.DecisionTreeBuilder();
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite() {
-        return new TestSuite(DecisionTree.class);
+    @Test
+    public void testLoadData() {
+        try {
+            builder.loadData("testdata.txt", 3, 4);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        Assert.assertArrayEquals(new Double[]{1.0, 2.0, 3.0}, builder.getData()[0]);
+        Assert.assertArrayEquals(new Double[]{4.0, 5.0, 6.0}, builder.getData()[1]);
+        Assert.assertArrayEquals(new Double[]{7.0, 8.0, 9.0}, builder.getData()[2]);
+        Assert.assertArrayEquals(new Double[]{10.0, 11.0, 12.0}, builder.getData()[3]);
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp() {
-        assertTrue(true);
-    }
+
 }
